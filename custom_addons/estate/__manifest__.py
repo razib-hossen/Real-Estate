f15223e87e7de09a9552adde4ec885323a70f306
Rajib Mahmud <rajib.conf@gmail.com>
2023-12-28 16:24:07 +0600

    added custom_addons with Chapter 3: A New Application

---
+++ custom_addons/estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Rajib Mahmud',
+    'category': 'Services',
+    'description': """
+    The Real Estate Advertisement module.
+    """,
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+}
