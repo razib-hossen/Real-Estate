d278f9051134d455a6efdc4dea1dfb6983adc9f9
Rajib Mahmud <rajib.conf@gmail.com>
2024-01-01 12:32:47 +0600

    organize the menus

--- custom_addons/estate/models/property.py
+++ custom_addons/estate/models/property.py
@@ -11,11 +11,11 @@ class Property(models.Model):
     expected_price = fields.Float("Expected Price", required=True)
     selling_price = fields.Float("Selling Price")
     bedrooms = fields.Integer("Bedrooms")
-    living_area = fields.Integer("Living Area")
+    living_area = fields.Integer("Living Area (sqm)")
     facades = fields.Integer("Facades")
     garage = fields.Boolean("Garage")
     garden = fields.Boolean("Garden")
-    garden_area = fields.Integer("Garden Area")
+    garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(
         string='Type',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
