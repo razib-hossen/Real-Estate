1493e69eca69ae9404939696bcc6b3f2c99370f5
Rajib Mahmud <rajib.conf@gmail.com>
2024-01-08 20:47:10 +0600

    added sequence to the estate.property.type list view with the correct widget.

--- estate/models/property_type.py
+++ estate/models/property_type.py
@@ -4,13 +4,14 @@ from odoo import models, fields
 class PropertyType(models.Model):
     _name = "estate.property.type"
     _description = "The Property Type Model"
-    _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char("Name")
 
     property_ids = fields.One2many(
         'estate.property', 'property_type_id', string="Properties"
     )
+    sequence = fields.Integer('Sequence', default=1, help="Used to order stages. Lower is better.")
 
     _sql_constraints = [
         ('unique_name', 'unique(name)', 'The name already created'),
